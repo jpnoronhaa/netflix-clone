487da06e42f8a3e99072c2129a23fe1b671beeba
Joao Pedro Noronha <joaopdrons@gmail.com>
2021-07-27 08:53:48 -0300

    connected to tmdb api & first page show the movies

--- src/App.js
+++ src/App.js
@@ -1,10 +1,46 @@
-import react from 'react';
+import { div } from 'prelude-ls';
+import React, { useEffect, useState } from 'react';
+import './App.css';
+import Tmdb from './Tmdb';
+import MovieRow from './components/MovieRow.js';
+import FeatureMovie from './components/FeatureMovie.js';
+
 
 const App = () => {
+
+  const [movieList, setMovieList] = useState([]);
+  const [featureData, setFeatureData] = useState(null)
+
+  useEffect(() => {
+    const loadAll = async () => {
+      let list = await Tmdb.getHomeList();
+      setMovieList(list);
+
+      let originals = list.filter(item=>item.slug === 'originals');
+      let randomChosen = Math.floor(Math.random() * (originals[0].items.results.length - 1));
+      let chosen = originals[0].items.results[randomChosen];
+      let chosenInfo = await Tmdb.getMovieInfo(chosen.id, 'tv');
+      setFeatureData(chosenInfo);
+     }
+
+     loadAll();
+  }, []);
+
   return (
-    <h1>
-      Hello World
-    </h1>
+    <div className="page">
+
+      {featureData &&
+        <FeatureMovie item={featureData} />
+      }
+
+      <section className="lists">
+        {
+          movieList.map((item, key) => (
+            <MovieRow key={key} title={item.title} items={item.items}/>
+          ))
+        }
+      </section>
+    </div>
   );
 };
 
